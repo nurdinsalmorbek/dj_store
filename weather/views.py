b3386d9a2807158697677e9810b36e0b96d3011b
nurdinsalmorbek <nurdinsalmorbek@gmail.com>
2023-03-15 16:34:02 +0600

    third commit

--- weather/views.py
+++ weather/views.py
@@ -1,11 +1,21 @@
+import requests
 from django.shortcuts import render
 
 def index(request):
     api_key = 'a6ec7b3179cd03a68214baaff63da702'
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=' + api_key
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + api_key
 
-    city = 'London'
+    city = 'Prague'
+    response = requests.get(url.format(city)).json()
+
+    city_info = {
+        'city': city,
+        'temp': response['main']['temp'],
+        'icon': response['weather'][0]['icon'],
+        'main': response['main']['humidity']
+    }
 
 
-    return render(request, 'weather/weather.html')
+    context = {'info': city_info}
+    return render(request, 'weather/weather.html', context)
 
